776e8fc6c79fc773614403bb2fea9fc71f2b4ca5
darrenbao1 <dbao@seraphlabs.io>
2022-11-21 23:16:05 +0800

    saving

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,14 @@ export default function Home() {
       <div className={styles.content}>
         <div className={styles.image}></div>
         <div className={styles.optionsContainer}>
-        <div className={styles.trait}>prescribe yourself</div>
+        <div className={styles.trait}>
+            <div>prescribe yourself</div>
+            <div>left arrow right arrow</div>
+            <div className={styles.name}>trait</div>
+          </div>
         <div className={styles.mintButton}>mint</div>
         <div className={styles.mintButton}>premium mint</div>
+
         </div>
       </div>
     </div>
