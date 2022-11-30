9193fac66099682895607b17efe86cf866e5388f
darrenbao1 <dbao@seraphlabs.io>
2022-11-30 22:27:32 +0800

    halfway wallet modal

--- components/ConnectWalletButton.tsx
+++ components/ConnectWalletButton.tsx
@@ -2,10 +2,11 @@ import { useAccount, useConnectors } from "@starknet-react/core";
 import { useEffect, useState } from "react";
 import { shortenAddress } from "../types/utils";
 import  ConnectMenuModal  from "./Modals/ConnectMenuModal";
-
+import styles from "../styles/ConnectWalletButton.module.css";
+import { TransactionList } from "./TransactionList";
 export const ConnectWalletButton = () => {
     const { account,address } = useAccount();
-    const { available, refresh } = useConnectors();
+    const { available, refresh, disconnect } = useConnectors();
     const [showConnectMenuModal, setShowConnectMenuModal] = useState(false);
     
     //useEffect to check the available wallet user has installed
@@ -20,8 +21,46 @@ export const ConnectWalletButton = () => {
         >
             connect wallet
             
-        </div> : <div className="connectWalletButton">{shortenAddress(address!)}</div>}
+        </div> : <ConnectedButton address={address!} disconnect={() => disconnect}/>}
         {showConnectMenuModal ? ( <ConnectMenuModal connectors={available} close={() => setShowConnectMenuModal(false)}/>) : null}
         </>
     )
 }
+
+const ConnectedButton = (props: {address: string,disconnect: any} ) => {
+    const [showDropDown, setShowDropDown] = useState(false);
+    const [showSnackBar, setShowSnackBar] = useState(false);
+    const CopyAddress = (walletAddress: string) => {
+        navigator.clipboard.writeText(walletAddress);
+        setShowSnackBar(true);
+        setTimeout(() => setShowSnackBar(false), 1000);
+    }
+    return (
+        <>
+    <div className="connectWalletButton" onClick={() => setShowDropDown(!showDropDown)}>
+        {shortenAddress(props.address)} &nbsp; <picture><img src="/downArrow.svg" alt="" style={showDropDown ? {transform:"rotateX(180deg)"} : {}}></img></picture>
+    </div>
+        {showDropDown && <div className={styles.container}>
+            <div className={styles.dropdownContainer}>
+                <div className={styles.dropdown}>
+                    <h1>my wallet</h1>
+                    <span>address</span>
+                    <div className={styles.addressContainer}>
+                        <div>{shortAddressForModal(props.address)}</div>
+                            <picture className={styles.copyIcon} onClick={() => CopyAddress(props.address)}>
+                                <img src="/copyIcon.svg" ></img>
+                            </picture> 
+                        </div>
+                    <TransactionList/>
+                    <div className={styles.disconnect} onClick={props.disconnect()}>disconnect wallet</div>
+                </div>
+               {showSnackBar && <div className={styles.snackbar}>copied</div>}
+            </div>
+        </div>}
+    </>)
+}
+
+function shortAddressForModal(string: string) {
+    if (string === undefined) return "unknown";
+    return string.substring(0, 23) + "..." + string.substring(string.length - 4);
+  }
---
+++ components/TransactionList.tsx
@@ -0,0 +1,20 @@
+import { useTransactionManager } from "@starknet-react/core"
+
+export const TransactionList = () => {
+    const { hashes } = useTransactionManager();
+    if (hashes.length == 0) {
+        return (
+          <div style={{ margin:"1rem 0"}}>
+            <h2>recent transactions</h2>
+            You have no recent transactions
+          </div>
+        );
+      }
+    return (
+        <div style={{ margin:"1rem 0"}}>
+            {hashes.map((data,index) => 
+                data && <div>something</div>).reverse()
+            }
+        </div>
+    )
+}
