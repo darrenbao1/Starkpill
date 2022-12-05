924fa5efd776ea0cacb89651512d89e1bf3bcd40
darrenbao1 <dbao@seraphlabs.io>
2022-12-05 19:25:59 +0800

    minting page traits section

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -2,51 +2,40 @@ import styles from "../styles/mint.module.css";
 import Image from "next/image";
 import { useAccount } from "@starknet-react/core";
 import { ConnectWalletButton } from "../components/ConnectWalletButton";
-export default function mint() {
+const Mint = () => {
 	const { account } = useAccount();
 	return (
 		<div className="container">
-			<div className={styles.backgroundFade}>
-				<div
-					className="contentContainer"
-					style={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-					}}
-				>
-					<div className={styles.leftContainer}>
-						<div className={styles.imageContainer}>
-							{/* base pill */}
-							<Image
-								src="/Base_StarkPill.PNG"
-								alt=""
-								width={365}
-								height={365}
-								className={styles.imageLayer}
-							></Image>
-							{/* face trait */}
-							<Image
-								src="/Braavos_Trait.PNG"
-								alt=""
-								width={365}
-								height={365}
-								className={styles.imageLayer}
-							></Image>
-						</div>
-						<div className={styles.traitsContainer}>
-							<h1>prescribe yourself!</h1>
-							<div className={styles.traitButton}> select facial trait</div>
-							<div className={styles.traitButton}> select background</div>
-							{!account ? (
-								<ConnectWalletButton />
-							) : (
-								<div className="connectWalletButton">mint</div>
-							)}
-						</div>
+			<div className={styles.backgroundFade}></div>
+			<div className="contentContainer">
+				<div className={styles.traitsContainer}>
+					<div className={styles.imageContainer}>
+						{/* base pill */}
+						<Image
+							src="/Base_StarkPill.PNG"
+							alt=""
+							fill
+							className={styles.imageLayer}
+						></Image>
+						{/* face trait */}
+						<Image
+							src="/Braavos_Trait.PNG"
+							alt=""
+							fill
+							className={styles.imageLayer}
+						></Image>
 					</div>
+					<div className={styles.header}>prescribe yourself!</div>
+					<div className={styles.button}>select facial traits</div>
+					<div className={styles.button}>select background</div>
+					{!account ? (
+						<ConnectWalletButton />
+					) : (
+						<div className="connectWalletButton">Mint</div>
+					)}
 				</div>
 			</div>
 		</div>
 	);
-}
+};
+export default Mint;
