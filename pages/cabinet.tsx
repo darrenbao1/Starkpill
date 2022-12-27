d7e564b4ab9654e77ee64ddcf1cdb72960bb90cd
darrenbao1 <dbao@seraphlabs.io>
2022-12-27 16:38:24 +0800

    testing cabinet

--- pages/cabinet.tsx
+++ pages/cabinet.tsx
@@ -8,40 +8,48 @@ import {
 } from "../types/constants";
 export default function Cabinet() {
 	const [jsonArray, setJsonArry] = useState<any[]>([]);
-	const offsetIncrement = 20;
+	const offsetIncrement = 100;
 	const [offsetAmount, setOffsetAmount] = useState(0);
+	const [isBottom, setIsBottom] = useState(false);
 	async function fetchData() {
-		await fetch(
-			MINTSQUARE_BASE_URL +
-				"nfts/" +
-				NETWORK_FOR_API +
-				"?collection=" +
-				STARKPILL_CONTRACT_ADDRESS +
-				"&limit=" +
-				offsetIncrement +
-				"&offset=" +
-				offsetAmount,
-			{
-				mode: "cors",
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-				},
-			}
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				setJsonArry((jsonArray) => [...jsonArray, ...data]);
-			});
+		try {
+			await fetch(
+				MINTSQUARE_BASE_URL +
+					"nfts/" +
+					NETWORK_FOR_API +
+					"?collection=" +
+					STARKPILL_CONTRACT_ADDRESS +
+					"&limit=" +
+					offsetIncrement +
+					"&offset=" +
+					offsetAmount,
+				{
+					method: "get",
+					mode: "cors",
+					headers: {
+						"Access-Control-Allow-Origin": "*",
+					},
+				}
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					setJsonArry((jsonArray) => [...jsonArray, ...data]);
+					setIsBottom(false);
+				});
+		} catch (e) {
+			console.log(e);
+		}
 	}
 	useEffect(() => {
 		fetchData();
-	}, []);
+	}, [isBottom]);
 
 	const handleScroll = (e: any) => {
 		const bottom =
 			e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
 		if (bottom) {
 			setOffsetAmount(offsetAmount + offsetIncrement);
+			setIsBottom(true);
 		}
 	};
 	return (
