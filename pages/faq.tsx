3c8b844ea2d454bcd8f16d9d4ac1d23d651ce857
bardon123 <brandonnmr18@gmail.com>
2023-01-06 10:39:08 +0800

    Added selection state and hover state to question

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -5,42 +5,51 @@ import { FAQ_QUESTIONS } from "../types/constants";
 import { FAQ } from "../types/interfaces";
 import { useState } from "react";
 export default function faq() {
-	const FaqItem = (props: { faq: FAQ }) => {
-		const [showAnswer, setShowAnswer] = useState(false);
-		return (
-			<div className={styles.questionContainer}>
-				<div
-					className={styles.question}
-					onClick={() => setShowAnswer(!showAnswer)}
-				>
-					{props.faq.question}
-					<div className={styles.button}>
-						{showAnswer ? <Cross /> : <Plus />}
-					</div>
-				</div>
-				{showAnswer && (
-					<>
-						<div className={styles.divider}></div>
-						<div className={styles.answer}>{props.faq.answer}</div>
-					</>
-				)}
-			</div>
-		);
-	};
+  const FaqItem = (props: { faq: FAQ }) => {
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [selectedQuestion, setSelectedQuestion] = useState(false);
 
-	return (
-		<div
-			className="container"
-			style={{ background: "white", color: "#000000" }}
-		>
-			<div className={styles.container}>
-				<h1>frequently asked questions</h1>
-				<ul>
-					{FAQ_QUESTIONS.map((faq, index) => (
-						<FaqItem faq={faq} key={index} />
-					))}
-				</ul>
-			</div>
-		</div>
-	);
+    return (
+      <div className={styles.questionContainer}>
+        <div
+          className={`${styles.question} ${
+            selectedQuestion === true ? styles.selectedQuestion : ""
+          }`}
+          onClick={() => {
+            setShowAnswer(!showAnswer);
+            setSelectedQuestion(!selectedQuestion);
+          }}
+        >
+          {props.faq.question}
+          <div className={styles.button}>
+            {showAnswer ? <Cross /> : <Plus />}
+          </div>
+        </div>
+        {showAnswer && (
+          <>
+            <div className={styles.divider}></div>
+            <div className={styles.answer}>{props.faq.answer}</div>
+          </>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className="container"
+      style={{ background: "white", color: "#000000" }}
+    >
+      <div className={styles.container}>
+        <h1 style={{ color: "#FF4F0A", fontSize: 40 }}>
+          frequently asked questions
+        </h1>
+        <ul>
+          {FAQ_QUESTIONS.map((faq, index) => (
+            <FaqItem faq={faq} key={index} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
