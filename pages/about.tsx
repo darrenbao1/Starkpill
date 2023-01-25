2213515487daf19e8815422e8527cfbcceb8fb6e
Brandon Ng Ming Ren <brandonngmingren@Brandons-MacBook-Pro.local>
2023-01-25 17:57:14 +0800

    faq page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,70 +1,76 @@
 import Link from "next/link";
-import styles from "../styles/about.module.css";
+import aboutStyles from "../styles/about.module.css";
 import Image from "next/image";
+import FAQ from "./faq";
 
 export default function about() {
-	return (
-		<div className="container" style={{ background: "#ffffff" }}>
-			<div className={styles.entireBox}>
-				<div className={styles.contentContainer}>
-					<h1 className={styles.aboutTextShift}> About Us</h1>
-					<Image
-						src="https://arweave.net/YUOO4tIjNdw82NlYv3reVJ4pQJw7uZFkm0LKKVdLSEA/TestPill/pill_004012.png"
-						className={styles.image}
-						height={500}
-						width={500}
-						alt=""
-					></Image>
-					<div className={styles.text}>
-						<h1 className={styles.headerText}>About us</h1>
-						<Link
-							href="https://twitter.com/GuthL/status/1594292390092750848?s=20&t=XaDDRn52fCJ5adU_GxeoHg"
-							target="_blank"
-						>
-							<u>Great memes</u>
-						</Link>
-						&nbsp;beget more memes, even if the resulting memes are mid.
-						<br />
-						<br />
-						Starkpills are NFTs built on the{" "}
-						<Link
-							href="https://twitter.com/211lp/status/1593622328771956737?s=20&t=XaDDRn52fCJ5adU_GxeoHg"
-							target="_blank"
-						>
-							<u>ERC-2114 Semi-Fungible Token Standard by Seraph Labs.</u>
-						</Link>
-						<br />
-						<br />
-						Each Starkpill is an NFT that owns/equips another NFT - the facial
-						expression.
-						<br />
-						<br />
-						Every facial expression is a unique NFT that can unequipped from a
-						Starkpill and changed for another one. <br />
-						<br />
-						However, at this time there are only 10 facial expressions and there
-						is no market to trade them. <br />
-						<br />
-						When we have the time, we will allow Starkpillers to
-						‘evolve’/customize their Starkpill by changing the facial expression
-						NFT that the Starkpill owns. More facial expressions may be added in
-						the future.
-						<br />
-						<br />
-						The Starkpill mint will officially end when StarkNet’s Mainnet is
-						fully live.
-						<br />
-						<br />
-						<Link
-							className="connectWalletButton"
-							style={{ width: "fit-content", color: "white", padding: "15px" }}
-							href="/mint"
-						>
-							Start Minting!
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="container"
+      style={{
+        background: `linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), #29296e`,
+      }}
+    >
+      <div className={aboutStyles.entireBox}>
+        <div className={aboutStyles.contentContainer}>
+          <h1 className={aboutStyles.aboutTextShift}>About</h1>
+          <Image
+            src="https://arweave.net/YUOO4tIjNdw82NlYv3reVJ4pQJw7uZFkm0LKKVdLSEA/TestPill/pill_004012.png"
+            className={aboutStyles.image}
+            height={558}
+            width={558}
+            alt=""
+          ></Image>
+          <div className={aboutStyles.text}>
+            <h1 className={aboutStyles.headerText} style={{ color: "#FF4F0A" }}>
+              {" "}
+              About
+            </h1>
+            <Link
+              href="https://twitter.com/GuthL/status/1594292390092750848?s=20&t=XaDDRn52fCJ5adU_GxeoHg"
+              target="_blank"
+            >
+              <u>Great memes</u>
+            </Link>
+            &nbsp;beget more memes
+            <br />
+            Starkpilled is an SFT (Semi-Fungible Token) collection of digital
+            avatars for people to celebrate to the StarkNet Ecosystem and
+            participate in the SFT movement.
+            <br />
+            Compared to traditional NFT collections that are of the ERC-721
+            token standard, Starkpills are ERC-2114s’ a Seraph Labs tweak of the
+            novel ERC-3525 developed by Solv Protocol. In short, SFTs are
+            dynamic “NFTs” that are able to be programmed and “equip” other
+            “NFTs”.
+            <br />
+            <h1 style={{ color: "#FF4F0A" }} className={aboutStyles.headerText}>
+              {" "}
+              Vision
+            </h1>
+            We want to showcase a better way to tokenize digital collectibles.
+            Second, we’d like to offer a different perspective on valuing PFP
+            collections - currently rarity is tied to the whole RNG’d PFP work
+            of art eg. you value CryptoPunk#2391 as it is. With Starkpilled the
+            idea is to value traits over the whole, so you can have a digital
+            avatar that you can identify with more meaningfully.
+            <br />
+            <br />
+            <Link
+              className="connectWalletButton"
+              style={{
+                width: "fit-content",
+                color: "white",
+                padding: "15px",
+              }}
+              href="/mint"
+            >
+              Start Minting!
+            </Link>
+          </div>
+        </div>
+      </div>
+      <FAQ></FAQ>
+    </div>
+  );
 }
