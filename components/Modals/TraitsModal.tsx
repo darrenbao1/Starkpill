85c17f051ab5e68d3e7b2140d3aea07adb989c74
darrenbao1 <dbao@seraphlabs.io>
2023-03-03 14:24:41 +0800

    added new label for price

--- components/Modals/TraitsModal.tsx
+++ components/Modals/TraitsModal.tsx
@@ -2,6 +2,7 @@ import styles from "../../styles/TraitsModal.module.css";
 import Cross from "../../public/svgs/cross.svg";
 import Image from "next/image";
 import { Trait } from "../../types/interfaces";
+import ETHLogo from "../../public/svgs/ethLogo.svg";
 export const TraitsModal = (props: {
 	traitName: string;
 	trait: Trait[];
@@ -44,7 +45,21 @@ export const TraitsModal = (props: {
 										? styles.centreTrait
 										: styles.centreTraitBackground
 								}></Image>
+							{trait.premiumPrice && (
+								<div
+									className={
+										index == props.selectedId
+											? styles.flagSelected
+											: styles.flag
+									}>
+									<span style={{ marginRight: "4px", marginLeft: "auto" }}>
+										{trait.premiumPrice}
+									</span>
+									<ETHLogo />
+								</div>
+							)}
 						</div>
+
 						<div
 							className={
 								index == props.selectedId
