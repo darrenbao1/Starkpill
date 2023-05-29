c132539856000b328a6e1fb8ac7174d88daa77ad
dbao <dbao@seraphlabs.io>
2023-05-29 17:42:35 +0800

    removing unused files

--- pages/mypills.tsx
+++ pages/mypills.tsx
@@ -2,18 +2,17 @@ import { useAccount } from "@starknet-react/core";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 import styles from "../styles/cabinet.module.css";
-// import { StarkPillCard } from "../components/StarkPillCard/"; <<Previous StarkPillCard
 import { StarkPillCard } from "../components/StarkPillCard/StarkPillCard";
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { convertToStandardWalletAddress } from "../types/utils";
-import { Disconnect } from "../components/Disconnect";
 import sharedBackgroundStyles from "../styles/sharedBackground.module.css";
 import { GET_USER_TOKENS, handleScrollToTop } from "../types/constants";
 import { BackToTopButton } from "../components/BackToTopButton";
 import Image from "next/image";
 import { ConnectWalletButton } from "../components/ConnectWalletButton";
+import { Disconnected } from "../components/DisconnectedPage.tsx/Disconnected";
 export default function Mypills() {
 	const refetchState = useSelector((state: any) => state.refetch);
 	const scrollTopRef = useRef<HTMLDivElement>(null);
@@ -100,53 +99,10 @@ export default function Mypills() {
 						)}
 				</div>
 			) : address &&
-			  walletAddress != convertToStandardWalletAddress(address!) ? (
-				<Disconnect address={convertToStandardWalletAddress(address!)} />
-			) : (
+			  walletAddress != convertToStandardWalletAddress(address!) ? null : (
 				<div
 					className={`container ${sharedBackgroundStyles.extendedBackground}`}>
-					<div
-						className="contentContainer"
-						style={{
-							height: "calc(100vh - 217px)",
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "center",
-						}}>
-						<div
-							style={{
-								display: "block",
-								alignItems: "center",
-								justifyContent: "center",
-								textAlign: "center",
-							}}>
-							<div style={{ fontSize: "32px" }}>
-								You have to connect your wallet before viewing your Starkpills
-							</div>
-							<div
-								style={{
-									marginTop: "40px",
-									width: "100%",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}>
-								<ConnectWalletButton />
-							</div>
-						</div>
-					</div>
-					<footer className={styles.footer}>
-						<a
-							href="https://www.seraphlabs.io/"
-							target="_blank"
-							rel="noreferrer">
-							<Image
-								src="/companyLogo.png"
-								height={56}
-								width={210}
-								alt="seraphLabs"></Image>
-						</a>
-					</footer>
+					<Disconnected text="You have to connect your wallet before viewing your Starkpills" />
 				</div>
 			)}
 		</>
