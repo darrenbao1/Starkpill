41533f41e7cc7a26f8c6a768f7266c391b96e656
dbao <dbao@seraphlabs.io>
2023-06-03 02:19:23 +0800

    hiding radbro

--- types/constants.tsx
+++ types/constants.tsx
@@ -622,13 +622,13 @@ export const COLLAB_PROJECTS: CollabProject[] = [
 		imageUrl: "/png/wassies.png",
 		redeemIngId: 33,
 	},
-	{
-		contract_address: "0xABCDB5710B88f456fED1e99025379e2969F29610",
-		name: "Radbro",
-		imageUrl: "/png/Radbro.png",
-		redeemIngId: 35,
-		redeemBgId: 20,
-	},
+	// {
+	// 	contract_address: "0xABCDB5710B88f456fED1e99025379e2969F29610",
+	// 	name: "Radbro",
+	// 	imageUrl: "/png/Radbro.png",
+	// 	redeemIngId: 35,
+	// 	redeemBgId: 20,
+	// },
 	{
 		contract_address: "0x5Af0D9827E0c53E4799BB226655A1de152A425a5",
 		name: "Milady",
