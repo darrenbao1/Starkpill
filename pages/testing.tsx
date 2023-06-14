0e767b7d07833d4ff5db04fb5b6583f85f93f4c9
bardon123 <brandonnmr18@gmail.com>
2023-06-14 13:57:34 +0800

    added scroll to kebab menu

---
+++ pages/testing.tsx
@@ -0,0 +1,9 @@
+import { RedemptionTraitModal } from "../components/Modals/RedemptionTraitModal/RedemptionTraitModal";
+
+export default function Testing() {
+	return (
+		<div>
+			<RedemptionTraitModal></RedemptionTraitModal>
+		</div>
+	);
+}
