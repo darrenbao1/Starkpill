cd14ddf6a17ef95df8e3fd23532b975a2913d35d
bardon123 <brandonnmr18@gmail.com>
2023-06-15 14:15:38 +0800

    deleted testing

--- pages/testing.tsx
+++
@@ -1,9 +0,0 @@
-import { RedemptionTraitModal } from "../components/Modals/RedemptionTraitModal/RedemptionTraitModal";
-
-export default function Testing() {
-	return (
-		<div>
-			<RedemptionTraitModal></RedemptionTraitModal>
-		</div>
-	);
-}
