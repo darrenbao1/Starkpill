6df82647606d650b7f25df5d9bfd4d91b8028f44
dbao <dbao@seraphlabs.io>
2023-06-16 18:04:47 +0800

    changed walletConnect to v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ const projectId = getWeb3ConnectId();
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiConfig = createConfig({
 	autoConnect: true,
-	connectors: w3mConnectors({ projectId, version: 1, chains }),
+	connectors: w3mConnectors({ projectId, version: 2, chains }),
 	publicClient,
 });
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
