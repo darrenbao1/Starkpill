a571fc0ff4c481cad465418962cd3b5b59b6ccb1
dbao <dbao@seraphlabs.io>
2023-07-07 00:08:14 +0800

    new fetching of eth method

--- hooks/StarkEthContract.tsx
+++ hooks/StarkEthContract.tsx
@@ -1,37 +1,24 @@
-import { useAccount, useContract, useContractRead } from "@starknet-react/core";
-import { Abi } from "starknet";
-
-import starkEthAbi from "../abi/starkEth.json";
-import { STARKETH_CONTRACT_ADDRESS } from "../types/constants";
+import { useAccount, useBalance } from "@starknet-react/core";
 import { useMemo } from "react";
-import { uint256ToBN } from "starknet/dist/utils/uint256";
-export function useStarkEthContract() {
-	return useContract({
-		abi: starkEthAbi as Abi,
-		address: STARKETH_CONTRACT_ADDRESS,
-	});
-}
+
 export function UserBalance() {
-	const { contract } = useStarkEthContract();
 	const { address } = useAccount();
-	const { data, isLoading, error } = useContractRead({
-		address: STARKETH_CONTRACT_ADDRESS,
-		abi: starkEthAbi as Abi,
-		functionName: "balanceOf",
-		args: [address],
-		watch: false,
+	const { data, isLoading, error } = useBalance({
+		address,
 	});
 	const ethBalance = useMemo(() => {
-		if (isLoading || !data?.length) {
+		if (isLoading) {
 			return <span>...</span>;
 		}
 		if (error) {
 			return <span>...</span>;
 		}
-		if (data && data.length > 0) {
-			let value = Number(uint256ToBN(data[0]));
-			value = value / Math.pow(10, 18);
-			return <span>{Number(value).toFixed(4).toString()}</span>;
+		if (data) {
+			return (
+				<span>
+					{data.formatted} {data.symbol}
+				</span>
+			);
 		} else {
 			return null;
 		}
