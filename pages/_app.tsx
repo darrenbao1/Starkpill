3139c059094da379fba1cac6deabbe560b4149e1
dbao <dbao@seraphlabs.io>
2023-07-17 14:16:38 +0800

    chatbot in about page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,15 +48,6 @@ export default function App({ Component, pageProps }: AppProps) {
 							<Head>
 								<link rel="icon" href="/starkpill.PNG"></link>
 								<title> getStarkpilled </title>
-
-								<script
-									dangerouslySetInnerHTML={{
-										__html: `
-            window.danteEmbed = "https://chat.dante-ai.com/embed?kb_id=a9bcad50-3f83-439d-8208-9822d3e20d78&token=d89ae89a-b19f-40dc-9a8b-07e5bc963f96&modeltype=gpt-4&mode=undefined&bubble=true&image=null&bubbleopen=false";
-          `,
-									}}
-								/>
-								<script src="https://chat.dante-ai.com/bubble-embed.js" />
 							</Head>
 							<ThemeProvider theme={StarkpillTheme}>
 								<Navbar />
