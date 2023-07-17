ec6ffa1c65e1143c1ce552a3a9ef9d5e598ada3d
dbao <dbao@seraphlabs.io>
2023-07-17 13:57:27 +0800

    added chatbot

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { AboutSection } from "../components/About/components/AboutSection";
 import { FaqSection } from "../components/About/components/FaqSection";
 export default function about() {
