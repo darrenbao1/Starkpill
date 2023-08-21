fb543e7d9e98db598e590b833e3c502af55060a5
dbao <dbao@seraphlabs.io>
2023-08-21 13:27:43 +0800

    hiding dante for now

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<script
-						async
-						dangerouslySetInnerHTML={{
-							__html: `
-                window.danteEmbed = "https://chat.dante-ai.com/embed?kb_id=a9bcad50-3f83-439d-8208-9822d3e20d78&token=0ed90700-ef6e-4cce-bbfd-f9db0fb02adc&modeltype=gpt-4&mode=undefined&bubble=true&image=null&bubbleopen=false";
-              `,
-						}}
-					/>
-					<script
-						async
-						src="https://chat.dante-ai.com/bubble-embed.js"
-						onLoad={() => {
-							// Additional code to execute after script is loaded (if needed)
-						}}
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+	// render() {
+	// 	return (
+	// 		<Html>
+	// 			<Head>
+	// 				<script
+	// 					async
+	// 					dangerouslySetInnerHTML={{
+	// 						__html: `
+	//             window.danteEmbed = "https://chat.dante-ai.com/embed?kb_id=a9bcad50-3f83-439d-8208-9822d3e20d78&token=0ed90700-ef6e-4cce-bbfd-f9db0fb02adc&modeltype=gpt-4&mode=undefined&bubble=true&image=null&bubbleopen=false";
+	//           `,
+	// 					}}
+	// 				/>
+	// 				<script
+	// 					async
+	// 					src="https://chat.dante-ai.com/bubble-embed.js"
+	// 					onLoad={() => {
+	// 						// Additional code to execute after script is loaded (if needed)
+	// 					}}
+	// 				/>
+	// 			</Head>
+	// 			<body>
+	// 				<Main />
+	// 				<NextScript />
+	// 			</body>
+	// 		</Html>
+	// 	);
+	// }
 }
 
 export default MyDocument;
