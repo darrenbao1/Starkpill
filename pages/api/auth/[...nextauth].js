1cff8342d13d7c905495d81659f5795920a2b55f
dbao <dbao@seraphlabs.io>
2023-09-04 16:24:00 +0800

    adding secret for nextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import TwitterProvider from "next-auth/providers/twitter";
 import NextAuth from "next-auth";
 export default NextAuth({
+	secret: process.env.SECRET,
 	providers: [
 		TwitterProvider({
 			clientId: process.env.TWITTER_CLIENT_ID,
