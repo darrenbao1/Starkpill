4ae1f64f87e4a0a4c87c11a4d67348f19aed0f8c
bardon123 <brandonnmr18@gmail.com>
2023-10-13 13:49:07 +0800

    mobile profile link

--- components/Modals/LinksModal.tsx
+++ components/Modals/LinksModal.tsx
@@ -3,13 +3,53 @@ import { useRouter } from "next/router";
 import styles from "../../styles/LinksModal.module.css";
 import { PAGES, USERPAGES } from "../../types/constants";
 import { useAccount } from "@starknet-react/core";
-import { convertToStandardWalletAddress } from "../../types/utils";
-export const LinksModal = (props: { close: any }) => {
+import {
+	convertToStandardWalletAddress,
+	shortenAddress,
+} from "../../types/utils";
+import { UserProfileBasic } from "../../types/interfaces";
+import Image from "next/image";
+
+export const LinksModal = (props: {
+	profilePictureUrl: string;
+	profile: UserProfileBasic;
+	close: () => void;
+}) => {
 	const router = useRouter();
 	const { address } = useAccount();
+	const openProfilePage = () => {
+		router.push({
+			pathname: "/profile",
+			query: { walletAddress: convertToStandardWalletAddress(address!) },
+		});
+		props.close();
+	};
+
 	return (
 		<div className={styles.modalContainer} style={{ zIndex: 1 }}>
 			<ul className={styles.modalItems}>
+				{address && props.profile && props.profile.transactions.length > 0 && (
+					<div
+						className={styles.profileLinkWrapper}
+						onClick={() => openProfilePage()}>
+						<div className={styles.profileIcon}>
+							<Image
+								src={props.profilePictureUrl || "/basepill.png"}
+								width={60}
+								height={60}
+								alt="ProfilePage"
+								style={{ borderRadius: "50%" }}
+								onClick={() => openProfilePage()}
+							/>
+						</div>
+
+						<h1>
+							{props.profile.username
+								? props.profile.username
+								: shortenAddress(props.profile.address)}
+						</h1>
+					</div>
+				)}
 				{PAGES.map(
 					(page, index) =>
 						page.isActive && (
